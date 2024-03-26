5563403261393758f7fb65525bc8c56113f2456f
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-03-26 06:36:25 +0300

    editting hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-std::string name;
-std::cin >> name;
-std::cout << "Hello world from " << name;
-return 0;
+   std::string name;
+   std::cin >> name;
+   std::cout << "Hello world from " << name;
+   return 0;
 }
